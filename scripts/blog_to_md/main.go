737949b58d16f964b68e18e5a088499d2a0a95a7
Christian Paez <christianpaeznegron@hotmail.com>
2026-04-04 17:50:35 -0500

    getting matches: href attributes

--- scripts/blog_to_md/main.go
+++ scripts/blog_to_md/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"regexp"
 )
 
 func main() {
@@ -17,4 +18,11 @@ func main() {
 		log.Printf("Error reading file: %v", err)
 	}
 	fmt.Printf("File contents: %s", contents)
+
+	validHrefRegex := regexp.MustCompile("href=\"([^\"]+)\"")
+	matches := validHrefRegex.FindAllStringSubmatch(string(contents), -1)
+	for _, value := range matches {
+		fmt.Printf("Match group found: %s\n", value[1])
+	}
+
 }
