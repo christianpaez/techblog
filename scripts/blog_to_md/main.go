a8e4fe02af475e9a210b30b8e165752c784cdafa
Christian Paez <christianpaeznegron@hotmail.com>
2026-04-04 16:22:20 -0500

    reading html file

--- scripts/blog_to_md/main.go
+++ scripts/blog_to_md/main.go
@@ -1,8 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+)
 
 func main() {
 
-	fmt.Println("Ready")
+	fmt.Println("Start program")
+	fmt.Println("Reading file...")
+
+	contents, err := os.ReadFile("index_example.html")
+
+	if err != nil {
+		log.Printf("Error reading file: %v", err)
+	}
+	fmt.Printf("File contents: %s", contents)
 }
