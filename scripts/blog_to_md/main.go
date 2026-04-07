dc08ec9dea441911e52ba7203aef295df7b987d4
Christian Paez <christianpaeznegron@hotmail.com>
2026-04-07 11:57:48 -0500

    visiting blog pages

--- scripts/blog_to_md/main.go
+++ scripts/blog_to_md/main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 	"regexp"
+	"strings"
 )
 
 func main() {
@@ -22,7 +24,19 @@ func main() {
 	validHrefRegex := regexp.MustCompile("href=\"([^\"]+)\"")
 	matches := validHrefRegex.FindAllStringSubmatch(string(contents), -1)
 	for _, value := range matches {
-		fmt.Printf("Match group found: %s\n", value[1])
+		s := []string{"https://dev.to", value[1]}
+		url := fmt.Sprintf(strings.Join(s, ""))
+		fmt.Printf("Visiting blog page: %s\n", url)
+		response, err := http.Get(url)
+
+		if err != nil {
+			panic(err)
+		}
+
+		defer response.Body.Close()
+
+		fmt.Printf("%v\n", response.Status)
+
 	}
 
 }
